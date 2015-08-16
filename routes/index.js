8213d8f397b3a8aa2065a22f70231ed132c46f8f
Kumar Anirudha <anirudhastark@yahoo.com>
2015-08-16 17:21:47 +0530

    iron router used. formatting screwed. Need to fix the stylesheet

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,16 @@
-Router.route('/', function () {
-  this.render('index');
+Router.map(function () {
+
+	// Home page
+	this.route('home', {
+		path: '/',
+		template: 'views/home'
+	});
+	
+	// Add new Notes
+	this.route('notes/add', {
+		path: 'notes/add',
+		template: 'views/notes'
+	});
+
 });
+

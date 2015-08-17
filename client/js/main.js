8738dbc88d69b58726f682db3d68ff41f431e3bd
T-Rex <thamizharasan123@gmail.com>
2015-08-17 18:06:49 +0530

    Added bootstrap, Jquery, made some UI changes

--- client/js/main.js
+++ client/js/main.js
@@ -1,1 +1,9 @@
 // Main Js file
+$(document).ready(function(){
+	var sideNavHeight = $(window).height();
+	$('.sideNav').height(sideNavHeight);
+}, $(document).resize(function(){
+	var sideNavHeight = $(window).height();
+	$('.sideNav').height(sideNavHeight);
+})
+);
